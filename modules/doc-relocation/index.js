235964e2461c02d6e9d0553b43961313a0589e07
draber <me@dieterraber.net>
2022-04-23 20:43:58 +0200

    update index.html location

--- modules/doc-relocation/index.js
+++ modules/doc-relocation/index.js
@@ -2,11 +2,11 @@ import pkg from "../../package.json" assert { type: "json" };
 import fs from "fs";
 
 fs.writeFile(
-    "./index.html",
+    "./docs/hybrid-object/index.html",
     `<!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="refresh" content="0;url=docs/hybrid-object/${pkg.version}/index.html" />
+    <meta http-equiv="refresh" content="0;url=${pkg.version}/index.html" />
 </head>
 </html>`,
     (err) => {
